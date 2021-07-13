20e0cd2c7bcd0b1f5b4d7b4a7872d89adeee0487
CarlssonOscar <oosccar@gmail.com>
2021-07-13 13:41:32 +0000

    Enable transition from information box to quizbox. Add questions.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ let start_btn = document.querySelector(".begin_button button");
 let information_box = document.querySelector(".information_box");
 let quit_btn = information_box.querySelector(".buttons .quit");
 let continue_btn = information_box.querySelector(".buttons .continue");
+let quiz_box = document.querySelector(".quiz_box");
 
 // If begin button
 start_btn.onclick = ()=>{
@@ -11,4 +12,10 @@ start_btn.onclick = ()=>{
 // If exit button
 quit_btn.onclick = ()=>{
     information_box.classList.remove("activeInfo"); //hide the information box
+}
+
+// If continue button
+continue_btn.onclick = ()=>{
+    information_box.classList.remove("activeInfo"); //hide the information box
+    quiz_box.classList.add("activeQuiz"); //show the quiz box
 }
