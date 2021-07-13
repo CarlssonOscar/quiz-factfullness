3f45672ffee5b9db00a0a17e9614dce22947eed6
CarlssonOscar <oosccar@gmail.com>
2021-07-13 16:05:06 +0000

    Add showQuestion function to enable transitions between questions

---
+++ assets/js/questions.js
@@ -0,0 +1,53 @@
+// Array questions
+let questions = [
+    {
+    numb: 1,
+    question: "What is the average life expectency of the worlds population?",
+    answer: "70 years",
+    options: [
+        "50 years",
+        "60 years",
+        "70 years"
+    ]
+},
+{
+    numb: 2,
+    question: "There is 2 billion children under 15 years old today. According to the UN, how many children will there be year 2100",
+    answer: "2 billion",
+    options: [
+        "4 billion",
+        "3 billion",
+        "2 billion"
+    ]
+},
+{
+    numb: 3,
+    question: "Globally 30 year old men have on average went to school for 10 years. What is the global average for women?",
+    answer: "9 years",
+    options: [
+        "9 years",
+        "6 years",
+        "3 years"
+    ]
+},
+{
+    numb: 4,
+    question: "During the latset 20 years has the amount of people living in extreme poverty ...?",
+    answer: "Almost been cut in half",
+    options: [
+        "Almost doubled",
+        "Remained the same",
+        "Almost been cut in half"
+    ]
+},
+{
+    numb: 5,
+    question: "How many people in the world has partial access to electricity?",
+    answer: "80 percent",
+    options: [
+        "20 percent",
+        "50 percent",
+        "80 percent"
+    ]
+}
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,50 @@
-let start_btn = document.querySelector(".begin_button button");
-let information_box = document.querySelector(".information_box");
-let quit_btn = information_box.querySelector(".buttons .quit");
-let continue_btn = information_box.querySelector(".buttons .continue");
-let quiz_box = document.querySelector(".quiz_box");
+const start_btn = document.querySelector(".begin_button button");
+const information_box = document.querySelector(".information_box");
+const quit_btn = information_box.querySelector(".buttons .quit");
+const continue_btn = information_box.querySelector(".buttons .continue");
+const quiz_box = document.querySelector(".quiz_box");
 
 // If begin button
-start_btn.onclick = ()=>{
+start_btn.onclick = () => {
     information_box.classList.add("activeInfo"); //show the information box
 }
 
 // If exit button
-quit_btn.onclick = ()=>{
+quit_btn.onclick = () => {
     information_box.classList.remove("activeInfo"); //hide the information box
 }
 
 // If continue button
-continue_btn.onclick = ()=>{
+continue_btn.onclick = () => {
     information_box.classList.remove("activeInfo"); //hide the information box
     quiz_box.classList.add("activeQuiz"); //show the quiz box
+    showQuestions(3);
+}
+
+let que_count = 0;
+
+const next_btn = quiz_box.querySelector(".next_btn");
+
+// If next button
+next_btn.onclick = ()=>{
+    if(que_count > questions.length -1){
+    que_count++;
+    showQuestions(que_count);
+    } else {
+        console.log("You have finished the questions!");
+    }
+}
+
+// Accessing questions from array
+function showQuestions(index) {
+    const que_text = document.querySelector(".que_text");
+    const option_list = document.querySelector(".option_list");
+    let que_tag = '<span>'+ questions[index].question +'</span>';
+
+    let option_tag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>' 
+                   + '<div class="option">'+ questions[index].options[1] +'<span></span></div>' 
+                   +'<div class="option">'+ questions[index].options[2] +'<span></span></div>';
+    que_text.innerHTML = que_tag;
+    option_list.innerHTML = option_tag;
+
 }
